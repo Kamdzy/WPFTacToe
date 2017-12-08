3517d4ce0fe20720ece0bfcff99495440ba89faa
Kamdzy <28660325+Kamdzy@users.noreply.github.com>
2017-12-08 12:29:41 +0100

    git Fresh start

--- WPFTacToe/MainWindow.xaml.cs
+++ WPFTacToe/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using MahApps.Metro.Controls;
 
 namespace WPFTacToe
@@ -21,77 +11,226 @@ namespace WPFTacToe
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        #region Fields
+
+        private Button[][] playerInputs;
+
+        private string playerMark = "X";
+        private int roundsLeft = 9;
+        #endregion
+
+        #region Constructor
 
         public MainWindow()
         {
             InitializeComponent();
-            GameStart();
+
+
+            playerInputs = new Button[3][]
+            {
+                new [] { Button0, Button1, Button2 },
+                new [] { Button3, Button4, Button5 },
+                new [] { Button6, Button7, Button8 },
+            };
+
+            GetChangelog();
         }
 
-        int currentPlayer = 1;
-        private string playerMark = "?";
-        private int roundsLeft = 9;
-        private List<Button> myButtons = new List<Button>();
-        public void ListButtons()
+        #endregion
+
+        private void GetChangelog()
         {
-            myButtons.Add(Button0);
-            myButtons.Add(Button1);
-            myButtons.Add(Button2);
-            myButtons.Add(Button3);
-            myButtons.Add(Button4);
-            myButtons.Add(Button5);
-            myButtons.Add(Button6);
-            myButtons.Add(Button7);
-            myButtons.Add(Button8);
+            var url = "https://raw.githubusercontent.com/Kamdzy/WPFTacToe/master/CHANGELOG.md";
+
+            using (var client = new WebClient())
+            {
+                Changelog.Text = client.DownloadString(url);
+            }
+
         }
 
-        public int RoundCheck()
+        public bool CheckWinCondition(string player)
         {
-            return 1;
+            if (CheckRow(player) || CheckColumn(player) || CheckDiagonal(player))
+            {
+                return true;
+            }
+
+            return false;
+
+
+        }
+
+        public bool CheckRow(string player)
+        {
+            for (int y = 0; y < playerInputs.Length; y++)
+            {
+                bool wholeRow = true;
+
+                for (int x = 0; x < playerInputs[y].Length && wholeRow; x++)
+                {
+                    if (playerInputs[x][y].Content as string != player)
+                    {
+                        wholeRow = false;
+                    }
+                }
+
+                if (wholeRow)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool CheckColumn(string player)
+        {
+            for (int x = 0; x < playerInputs.Length; x++)
+            {
+                bool wholeColumn = true;
+
+                for (int y = 0; y < playerInputs[x].Length && wholeColumn; y++)
+                {
+                    if (playerInputs[x][y].Content as string != player)
+                    {
+                        wholeColumn = false;
+                    }
+                }
+
+                if (wholeColumn)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool CheckDiagonal(string player)
+        {
+            bool wholeDiaognal = true;
+
+            for (int x = 0; x < playerInputs[0].Length && wholeDiaognal; x++)
+            {
+                if ((playerInputs[x][x].Content as string != player) 
+                    && (playerInputs[x][playerInputs.Length - 1 - x].Content as string != player))
+                {
+                    wholeDiaognal = false;
+                }
+            }
+
+            if (wholeDiaognal)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public void PlayerSwitch()
         {
-            if (currentPlayer == 1)
+            if (playerMark == "O")
             {
+                Turn_Label.Content = "Turn: Player 1 ";
                 playerMark = "X";
-                currentPlayer = 2;
             }
-            else if (currentPlayer == 2)
+            else if (playerMark == "X")
             {
+                Turn_Label.Content = "Turn: Player 2 ";
                 playerMark = "O";
-                currentPlayer = 1;
-            }
-        }
-
-        public void PlayMove()
-        {
-            foreach (var button in myButtons)
-            {
-                button.Click += Button_Click;
-            }
-        }
-
-        public void GameStart()
-        {
-            ListButtons();
-
-            while (roundsLeft >= 0)
-            {
-                PlayMove();
-
-                if (RoundCheck() == 0)
-                {
-                    continue;
-                }
-
-                roundsLeft--;
             }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.IsEnabled = false;
+            if (sender is Button)
+            {
+                roundsLeft--;
+                var button = (Button)sender;
+
+
+                button.Content = playerMark;
+                button.IsEnabled = false;
+
+                if (CheckWinCondition("X"))
+                {
+                    Turn_Label.Content = "Player 1 won.";
+                    PlayFieldGrid.IsEnabled = false;
+                }
+                else if (CheckWinCondition("O"))
+                {
+                    Turn_Label.Content = "Player 2 won.";
+                    PlayFieldGrid.IsEnabled = false;
+                }
+                else
+                {
+                    if (roundsLeft <= 0)
+                    {
+                        Turn_Label.Content = "It's a draw.";
+                        PlayFieldGrid.IsEnabled = false;
+                    }
+                    else
+                    {
+                        PlayerSwitch();
+                    }
+                }
+            }
         }
+
+        private void Restart_Click(object sender, RoutedEventArgs e)
+        {
+            playerMark = "X";
+            roundsLeft = 9;
+            Turn_Label.Content = "Turn: Player 1";
+
+            PlayFieldGrid.IsEnabled = true;
+
+            foreach (var btn in playerInputs.SelectMany(x => x))
+            {
+                btn.IsEnabled = true;
+                btn.Content = "";
+            }
+        }
+
+        #region Window commands
+
+        private void Main_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.SelectionGrid.Visibility = Visibility.Visible;
+            this.GameGrid.Visibility = Visibility.Collapsed;
+            this.AboutGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void Game_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.SelectionGrid.Visibility = Visibility.Collapsed;
+            this.GameGrid.Visibility = Visibility.Visible;
+            this.AboutGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.SelectionGrid.Visibility = Visibility.Collapsed;
+            this.GameGrid.Visibility = Visibility.Collapsed;
+            this.AboutGrid.Visibility = Visibility.Visible;
+        }
+
+
+        private void PvPL_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.SelectionGrid.Visibility = Visibility.Collapsed;
+            this.GameGrid.Visibility = Visibility.Visible;
+            this.AboutGrid.Visibility = Visibility.Collapsed;
+
+            GameButton.IsEnabled = true;
+        }
+
+        #endregion
+        
     }
 }
